7b4406d0979851bd0c0788f10a3306108f6e4248
kevinyu0506 <kevinyu05062006@gmail.com>
2021-09-02 17:19:51 +0800

    Update shell script

--- push_image.sh
+++ push_image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker buildx build --platform linux/amd54,linux/arm64 -t kevinyu0506/update-kustomize-image:v1.3 --push .
+docker buildx build --platform linux/amd64,linux/arm64 -t kevinyu0506/update-kustomize-image:v1.3 --push .
