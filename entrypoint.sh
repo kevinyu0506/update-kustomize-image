90c64d37ea5fed22c0f8e50e9f047a2a7758a131
kevinyu0506 <kevinyu05062006@gmail.com>
2021-09-02 16:16:41 +0800

    Revamp Dockerfile & entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,7 @@ git clone $REMOTE_REPO k8s-manifests
 cd k8s-manifests
 
 if [ ! -z "$IMAGE" ] && [ ! -z "$IMAGE_TAG" ]; then
-    echo "Updating image $IMAGE to $IMAGE:$IMAGE_TAG..."
+    echo "Updating $BUILD_PHASE image $IMAGE to $IMAGE:$IMAGE_TAG..."
     update_image_tag $IMAGE $IMAGE_TAG "$KUSTOMIZE_PATH/kustomization.yaml"
 else
     echo "Missing specify image to update"
@@ -27,9 +27,9 @@ git config user.name "$GITHUB_USER"
 git config user.email "$GITHUB_USER_EMAIL"
 
 git add .
-git commit -m "[Cloud Build] Updating $IMAGE to $IMAGE:$IMAGE_TAG"
+git commit -m "[Cloud Build][$BUILD_PHASE] Updating $IMAGE to $IMAGE:$IMAGE_TAG"
 
 git push origin master
 
 rm -rf k8s-manifests
-echo "New version deployed!"
+echo "New $BUILD_PHASE version deployed!"
--- push_image.sh
+++ push_image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker buildx build --platform linux/amd54,linux/arm64 -t kevinyu0506/update-kustomize-image:v1.2 --push .
+docker buildx build --platform linux/amd54,linux/arm64 -t kevinyu0506/update-kustomize-image:v1.3 --push .
